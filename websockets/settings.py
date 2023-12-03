3271fc53b69966cc17539a01361b584ea8db1ba8
Henry Cortez <henrystevencortez@yahoo.es>
2023-12-02 22:17:38 -0500

    se a;adop cosas

--- websockets/settings.py
+++ websockets/settings.py
@@ -159,10 +159,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
