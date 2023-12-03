7a1c2ed34332950c58347700c24be0405a87fafa
Henry Cortez <henrystevencortez@yahoo.es>
2023-12-02 21:30:49 -0500

    se cambios

--- websockets/settings.py
+++ websockets/settings.py
@@ -73,7 +73,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [(os.getenv("REDIS_HOST", "localhost"), int(os.getenv("REDIS_PORT", 6379)))],
         },
     },
 }
